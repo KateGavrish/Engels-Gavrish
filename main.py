567859e86ea196facdc141a7420104a9c02172b1
gavrishekaterina <ketrin2303@yandex.ru>
2019-11-09 19:33:47 +0400

    last commit

--- main.py
+++ main.py
@@ -2,31 +2,39 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
-from PyQt5.QtCore import Qt, QPointF
+from PyQt5 import QtCore
 import random
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main1.ui', self)
-        self.pushButton.clicked.connect(self.paintEvent)
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.buttn)
+        self.draw1 = False
 
-    def paintEvent(self):
+    def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         self.draw(qp)
-        self.update()
+        if self.draw1:
+            self.update()
+            self.draw1 = False
         qp.end()
 
     def draw(self, qp):
         s = random.randint(4, 50)
+        x = random.randint(0, 700)
+        y = random.randint(0, 500)
         qp.setBrush(QColor(245, 224, 66))
-        qp.drawEllipse(self.x, self.y, s, s)
+        qp.drawEllipse(x, y, s, s)
 
+    def buttn(self):
+        self.draw1 = True
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
+    ex.show()
     sys.exit(app.exec_())
